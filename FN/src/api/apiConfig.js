a77f9567027cf07786b68ef0a670690990f3f261
junwoogyun <jwg8910@naver.com>
2025-02-07 11:02:53 +0900

    ApiConfig Validation Check Interval 적용(15) 테스트 전

--- FN/src/api/apiConfig.js
+++ FN/src/api/apiConfig.js
@@ -6,6 +6,10 @@ const api = axios.create({
   withCredentials: true, // HTTP-Only 쿠키 포함
 });
 
+// 마지막 인증 검증 시간을 저장
+let lastValidationTime = 0;
+const VALIDATION_INTERVAL = 1000 * 15; // 15초로 수정
+
 //------------------------
 // 요청 인터셉터 설정
 //------------------------
@@ -17,22 +21,23 @@ api.interceptors.request.use(
       return config;
     }
 
-    try {
-      
-      // 토큰 유효성 검증을 위한 별도 엔드포인트 호출
-      await axios.get('/bn/validate', {
-        withCredentials: true
-      });
-      console.log("[정상-요청 인터셉터] 인증된 상태입니다");
-      return config;
-
-    } catch (error) {
-      console.log("[오류-요청 인터셉터] ",error);
-      window.location.href = '/#/login';
-
-      return Promise.reject('인증이 필요합니다.');
-      
+    const now = Date.now();
+    // 마지막 검증 이후 5분이 지났을 때만 새로운 검증 요청
+    if (now - lastValidationTime > VALIDATION_INTERVAL) {
+      try {
+        await axios.get('/bn/validate', {
+          withCredentials: true
+        });
+        lastValidationTime = now;
+        console.log("[정상-요청 인터셉터] 인증된 상태입니다");
+      } catch (error) {
+        console.log("[오류-요청 인터셉터] ", error);
+        window.location.href = '/#/login';
+        return Promise.reject('인증이 필요합니다.');
+      }
     }
+
+    return config;
   },
   (error) => {
     console.log("[오류-요청 인터셉터] ",error);
@@ -46,17 +51,16 @@ api.interceptors.request.use(
 //------------------------
 api.interceptors.response.use(
   (response) => {
-    console.log("[정상-응답 인터셉터] ",response);
     if (response.data?.auth === false) {
+      lastValidationTime = 0; // 인증 실패시 다음 요청에서 재검증하도록 초기화
       window.location.href = '/#/login';
       return Promise.reject('세션이 만료되었습니다.');
     }
     return response;
   },
   (error) => {
-  
-    console.log("[오류-응답 인터셉터] ",error);
     if (error.response?.data?.expired === true) {
+      lastValidationTime = 0; // 인증 실패시 다음 요청에서 재검증하도록 초기화
       window.location.href = '/#/login';
     }
     return Promise.reject(error);
