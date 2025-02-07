246ed9dc6a9cce7744888e40c2c9047f90c52c7c
junwoogyun <jwg8910@naver.com>
2025-02-07 17:11:35 +0900

    apiconfig interceptor 문제로 다시 설정 할것

--- FN/src/api/apiConfig.js
+++ FN/src/api/apiConfig.js
@@ -13,58 +13,58 @@ const VALIDATION_INTERVAL = 1000 * 15; // 15초로 수정
 //------------------------
 // 요청 인터셉터 설정
 //------------------------
-api.interceptors.request.use(
-  async (config) => {
-    // 로그인 페이지나 회원가입 페이지 등 인증이 필요없는 경로는 제외
-    const publicPaths = ['/#/login', '/#/join','/login','/bn/validate','/validate'];
-    if (publicPaths.some(path => config.url.includes(path))) {
-      return config;
-    }
+// api.interceptors.request.use(
+//   async (config) => {
+//     // 로그인 페이지나 회원가입 페이지 등 인증이 필요없는 경로는 제외
+//     const publicPaths = ['/#/login', '/#/join','/login','/bn/validate','/validate'];
+//     if (publicPaths.some(path => config.url.includes(path))) {
+//       return config;
+//     }
 
-    const now = Date.now();
-    // 마지막 검증 이후 5분이 지났을 때만 새로운 검증 요청
-    if (now - lastValidationTime > VALIDATION_INTERVAL) {
-      try {
-        await axios.get('/bn/validate', {
-          withCredentials: true
-        });
-        lastValidationTime = now;
-        console.log("[정상-요청 인터셉터] 인증된 상태입니다");
-      } catch (error) {
-        console.log("[오류-요청 인터셉터] ", error);
-        window.location.href = '/#/login';
-        return Promise.reject('인증이 필요합니다.');
-      }
-    }
+//     const now = Date.now();
+//     // 마지막 검증 이후 5분이 지났을 때만 새로운 검증 요청
+//     if (now - lastValidationTime > VALIDATION_INTERVAL) {
+//       try {
+//         await axios.get('/bn/validate', {
+//           withCredentials: true
+//         });
+//         lastValidationTime = now;
+//         console.log("[정상-요청 인터셉터] 인증된 상태입니다");
+//       } catch (error) {
+//         console.log("[오류-요청 인터셉터] ", error);
+//         window.location.href = '/#/login';
+//         return Promise.reject('인증이 필요합니다.');
+//       }
+//     }
 
-    return config;
-  },
-  (error) => {
-    console.log("[오류-요청 인터셉터] ",error);
-    window.location.href = '/#/login';
-    return Promise.reject(error);
-  }
-);
+//     return config;
+//   },
+//   (error) => {
+//     console.log("[오류-요청 인터셉터] ",error);
+//     window.location.href = '/#/login';
+//     return Promise.reject(error);
+//   }
+// );
 
-//------------------------
-// 응답 인터셉터 설정
-//------------------------
-api.interceptors.response.use(
-  (response) => {
-    if (response.data?.auth === false) {
-      lastValidationTime = 0; // 인증 실패시 다음 요청에서 재검증하도록 초기화
-      window.location.href = '/#/login';
-      return Promise.reject('세션이 만료되었습니다.');
-    }
-    return response;
-  },
-  (error) => {
-    if (error.response?.data?.expired === true) {
-      lastValidationTime = 0; // 인증 실패시 다음 요청에서 재검증하도록 초기화
-      window.location.href = '/#/login';
-    }
-    return Promise.reject(error);
-  }
-);
+// //------------------------
+// // 응답 인터셉터 설정
+// //------------------------
+// api.interceptors.response.use(
+//   (response) => {
+//     if (response.data?.auth === false) {
+//       lastValidationTime = 0; // 인증 실패시 다음 요청에서 재검증하도록 초기화
+//       window.location.href = '/#/login';
+//       return Promise.reject('세션이 만료되었습니다.');
+//     }
+//     return response;
+//   },
+//   (error) => {
+//     if (error.response?.data?.expired === true) {
+//       lastValidationTime = 0; // 인증 실패시 다음 요청에서 재검증하도록 초기화
+//       window.location.href = '/#/login';
+//     }
+//     return Promise.reject(error);
+//   }
+// );
 
 export default api; 
